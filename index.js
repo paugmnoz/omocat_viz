d2c89300088526381627aaffa41898762da50da6
Paula <paulaagarcia1@gmail.com>
2018-05-29 23:56:17 -0500

    data added

---
+++ index.js
@@ -0,0 +1,35 @@
+//width and height define
+var width = 960,
+    height = 500,
+    active = d3.select(null);
+
+//append svg element to html body with height and width
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+svg.append("rect")
+    .attr("class", "background")
+    .attr("width", width)
+    .attr("height", height);
+
+//to make groups
+var g = svg.append("g");
+
+//select colors for data viz (dots)
+var colors = d3.scale.category10();
+
+//add database for viz
+d3.csv("./resources/artistsDB.csv", function (error, artists) {
+    d3.json('./resources/omocat_logo_coordinator.json', function (error, logoCoords) {
+
+        g.selectAll("circle").data(artists)
+            .enter().append("circle")
+            .attr("cx", (d, i) => logoCoords[i][0])
+            .attr("cy", (d, i) => logoCoords[i][1] - 200)
+            .attr("r", 2)
+
+
+    });
+
+});
